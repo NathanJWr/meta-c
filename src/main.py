6c5e367130b19e936f024172a752b53bc3f1246d
NathanJWr <nathanielwright@protonmail.com>
2019-10-22 08:54:44 -0700

    there is now a list of tokens that has the info needed to output the program

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from typing import Union
+from typing import Tuple
+from typing import List
 
 class Token(Enum):
     eof = -1,
@@ -10,15 +12,31 @@ class Token(Enum):
     constant = -6
 
 last_char: str = ' '
-identifier_string: str
+identifier_string: str = ""
+num_string: str
 current_line = 0
+num_tabs = 0
+cur_tok: Union[Token, str]
 cur_file = open("test3.c", 'r')
+
+def nexttok() -> Tuple[Union[Token, str], str]:
+    global cur_tok, identifier_string
+    cur_tok = gettok()
+    return cur_tok, identifier_string
+
 def gettok() -> Union[Token, str]:
-    global last_char, current_line
+    global last_char, current_line, num_tabs, identifier_string
 
     if last_char == "\n":
-        current_line += current_line
-    if last_char.isalpha():
+        current_line += 1
+
+    elif last_char == "{":
+        num_tabs += 1
+
+    elif last_char == "}":
+        num_tabs -= 1
+
+    elif last_char.isalpha():
         identifier_string = "";
         while last_char.isalnum():
             identifier_string += last_char
@@ -26,12 +44,18 @@ def gettok() -> Union[Token, str]:
         if identifier_string == "vector":
             return Token.vector
         elif identifier_string == "typedef":
-            return Token.typdef
+            return Token.typedef
         else:
             return Token.identifier
 
+    elif last_char.isdigit():
+        identifier_string = "";
+        while last_char.isdigit() or last_char == ".":
+            identifier_string += last_char
+            last_char = cur_file.read(1)
+        return Token.constant
 
-    if not last_char:
+    elif not last_char:
         return Token.eof
 
     this_char: str = last_char
@@ -39,10 +63,13 @@ def gettok() -> Union[Token, str]:
     return this_char
     
 
+token_list: List[Tuple[Union[Token, str], str]] = []
+nexttok()
 while (1):
-    val = gettok()
-    if val == Token.eof:
+    val = nexttok()
+    token_list.append(val)
+    if val[0] == Token.eof:
         break
 
-    print(val)
+print(token_list)
 
