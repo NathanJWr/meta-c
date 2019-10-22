878d5c236bd0e91620a08e80326ce3bee8ea8997
NathanJWr <nathanielwright@protonmail.com>
2019-10-22 11:07:29 -0700

    gettok now only returns a Token

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ class Token(Enum):
     template = -4
     typedef = -5
     constant = -6
+    char = -7
 
 last_char: str = ' '
 identifier_string: str = ""
@@ -19,14 +20,16 @@ num_tabs = 0
 cur_tok: Union[Token, str]
 cur_file = open("test3.c", 'r')
 
-def nexttok() -> Tuple[Union[Token, str], str]:
+def nexttok() -> Tuple[Token, str]:
     global cur_tok, identifier_string
     cur_tok = gettok()
     return cur_tok, identifier_string
 
-def gettok() -> Union[Token, str]:
+def gettok() -> Token:
     global last_char, current_line, num_tabs, identifier_string
+    this_char: str
 
+    identifier_string = ""
     if last_char == "\n":
         current_line += 1
 
@@ -37,7 +40,6 @@ def gettok() -> Union[Token, str]:
         num_tabs -= 1
 
     elif last_char.isalpha():
-        identifier_string = "";
         while last_char.isalnum():
             identifier_string += last_char
             last_char = cur_file.read(1)
@@ -49,7 +51,6 @@ def gettok() -> Union[Token, str]:
             return Token.identifier
 
     elif last_char.isdigit():
-        identifier_string = "";
         while last_char.isdigit() or last_char == ".":
             identifier_string += last_char
             last_char = cur_file.read(1)
@@ -58,12 +59,12 @@ def gettok() -> Union[Token, str]:
     elif not last_char:
         return Token.eof
 
-    this_char: str = last_char
+    identifier_string = last_char
     last_char = cur_file.read(1)
-    return this_char
+    return Token.char
     
 
-token_list: List[Tuple[Union[Token, str], str]] = []
+token_list: List[Tuple[Token, str]] = []
 nexttok()
 while (1):
     val = nexttok()
