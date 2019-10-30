673ac5f1f3470021687b4270d6382d831b8dcbc4
NathanJWr <nathanielwright@protonmail.com>
2019-10-30 16:27:22 -0700

    support for a list of files instead of just one

--- src/meta.py
+++ src/meta.py
@@ -4,37 +4,48 @@ from output import Output
 from c_tokenizer import Tokenizer
 
 from collections import deque
-from typing import Deque
+from typing import Deque, TextIO
 
 
-cur_file = open("test.c", 'r')
+
+
+file_list = ["test.c", "test2.c"]
 source_file_count = 0
-token_list: Deque[CToken] = deque()
-tokenizer = Tokenizer()
-tokenizer.nexttok(cur_file)
+
+def output_to_file(output: Output, cur_file: TextIO) -> None:
+    global source_file_count
+
+    output_file_name = "__" + cur_file.name
+    vector_file_name = "__vector" + str(source_file_count) + ".h"
+    output_file = open(output_file_name, 'w')
+    vector_file = open(vector_file_name, 'w')
+
+    output_file.write(output.global_out)
+    output_file.write(output.normal_out)
+
+    vector_file.write(output.vector_out)
+
+    vector_file.close()
+    output_file.close()
+    source_file_count += 1
 
 
-while (1):
-    tok = tokenizer.nexttok(cur_file)
-    token_list.append(tok)
-    if tok.val == Tok.eof:
-        break
+while source_file_count < len(file_list):
+    cur_file = open(file_list[source_file_count], 'r')
+    token_list: Deque[CToken] = deque()
+    tokenizer = Tokenizer()
+    tokenizer.nexttok(cur_file)
+    while (1):
+        tok = tokenizer.nexttok(cur_file)
+        token_list.append(tok)
+        if tok.val == Tok.eof:
+            break
 
-output = Output()
-parse(output, token_list)
+    output = Output()
+    parse(output, token_list)
 
-# output to files
-output_file_name = "__" + cur_file.name
-vector_file_name = "__vector" + str(source_file_count) + ".h"
-output_file = open(output_file_name, 'w')
-vector_file = open(vector_file_name, 'w')
-
-output_file.write(output.global_out)
-output_file.write(output.normal_out)
-
-vector_file.write(output.vector_out)
-
-vector_file.close()
-output_file.close()
+    # output to files
+    output_to_file(output, cur_file)
+    cur_file.close()
 
 
